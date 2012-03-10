072f6b553dd7432a3015f9317028153ff79e44d9
belovgarik@mail.ru <belovgarik@mail.ru@97b8eb37-9640-a43b-0733-806e0cc61b54>
2012-03-10 12:11:33 +0000

    работа со скриптом

--- xo/xo/CheckComputer.lua
+++ xo/xo/CheckComputer.lua
@@ -1,10 +1,31 @@
 Field = {}
 
 function IO()
-    Field = SaveField()
+    Field = SaveField()	
+	local x,y
+	for i = 1, 3 do
+		local j = 0;
+		if Field[i+j] == 1 and Field[i+j+2] == 1 then
+			x = 1
+			y = i - 1
+			break
+		end
+		if Field[i+j] == 1 and Field[i+j+1] == 1 then
+			x = 2
+			y = i - 1
+			break
+		end
+		if Field[i+j+1] == 1 and Field[i+j+2] == 1 then
+			x = 0
+			y = i - 1
+			break
+		end
+		j = j + 2
+	end
 	if Field[9] == 10 then
 		x = 2
 		y = 2
 	end
+	a,b = x,y
 	return x,y	
 end
