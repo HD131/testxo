fd6fee189fcf6e4e24c86d2485c7be1e1a9dfa3d
belovgarik <belovgarik@mail.ru>
2012-03-14 10:14:58 +0000

    доработка классов

--- Vector/Vector/Class.cpp
+++ Vector/Vector/Class.cpp
@@ -1,5 +1,6 @@
 #include "Class.h"
 
+//---------------------float-----------------------------
 Vec3f Vec3f::operator + ( const Vec3f &v) const
 {	
 	return Vec3f( x + v.x, y + v.y, z + v.z );
@@ -9,6 +10,7 @@ Vec3f Vec3f::operator - ( const Vec3f &v) const
 {	
 	return Vec3f( x + v.x, y + v.y, z + v.z );
 }
+
 Vec3f Vec3f::operator * ( float a ) const
 {	
 	return Vec3f( x * a, y * a, z * a );
@@ -18,3 +20,74 @@ Vec3f Vec3f::operator / ( float a ) const
 {	
 	return Vec3f( x / a, y / a, z / a );
 }
+
+Vec3f Vec3f::operator + ( float a ) const
+{	
+	return Vec3f( x + a, y + a, z + a );
+}
+
+Vec3f Vec3f::operator - ( float a ) const
+{	
+	return Vec3f( x - a, y - a, z - a );
+}
+//----------------------int----------------------------
+Vec3i Vec3i::operator + ( const Vec3i &v) const
+{	
+	return Vec3i( x + v.x, y + v.y, z + v.z );
+}
+
+Vec3i Vec3i::operator - ( const Vec3i &v) const
+{	
+	return Vec3i( x + v.x, y + v.y, z + v.z );
+}
+
+Vec3i Vec3i::operator * ( int a ) const
+{	
+	return Vec3i( x * a, y * a, z * a );
+}
+
+Vec3i Vec3i::operator / ( int a ) const
+{	
+	return Vec3i( x / a, y / a, z / a );
+}
+
+Vec3i Vec3i::operator + ( int a ) const
+{	
+	return Vec3i( x + a, y + a, z + a );
+}
+
+Vec3i Vec3i::operator - ( int a ) const
+{	
+	return Vec3i( x - a, y - a, z - a );
+}
+//---------------------double-----------------------------
+Vec3d Vec3d::operator + ( const Vec3d &v) const
+{	
+	return Vec3d( x + v.x, y + v.y, z + v.z );
+}
+
+Vec3d Vec3d::operator - ( const Vec3d &v) const
+{	
+	return Vec3d( x + v.x, y + v.y, z + v.z );
+}
+
+Vec3d Vec3d::operator * ( double a ) const
+{	
+	return Vec3d( x * a, y * a, z * a );
+}
+
+Vec3d Vec3d::operator / ( double a ) const
+{	
+	return Vec3d( x / a, y / a, z / a );
+}
+
+Vec3d Vec3d::operator + ( double a ) const
+{	
+	return Vec3d( x + a, y + a, z + a );
+}
+
+Vec3d Vec3d::operator - ( double a ) const
+{	
+	return Vec3d( x - a, y - a, z - a );
+}
+//--------------------------------------------------
--- Vector/Vector/Vector.cpp
+++ Vector/Vector/Vector.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-	Vec3f v1(1,2,3);
-	Vec3f v2(1,2,3);
-	Vec3f v3 = v1 / 2;
+	Vec3i v1(1,2,3);
+	Vec3i v2(1,2,3);
+	Vec3i v3 = v1 + 2;
 
 	cout << v3.x << "\t" << v3.y << "\t" << v3.z;
 	cin.get();
