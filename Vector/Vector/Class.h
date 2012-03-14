de62d649120bd8b88f6bf07176e3cd95605f07a0
belovgarik@mail.ru <belovgarik@mail.ru@97b8eb37-9640-a43b-0733-806e0cc61b54>
2012-03-14 10:14:58 +0000

    доработка классов

--- Vector/Vector/Class.h
+++ Vector/Vector/Class.h
@@ -1,3 +1,5 @@
+#pragma once
+
 typedef class Vec3f
 {
 public:
@@ -14,5 +16,47 @@ public:
 	Vec3f operator - ( const Vec3f& ) const;
 	Vec3f operator * ( float ) const;
 	Vec3f operator / ( float ) const;
+	Vec3f operator + ( float ) const;
+	Vec3f operator - ( float ) const;
+};
+
+typedef class Vec3i
+{
+public:
+	int x;
+	int y;
+	int z;
+	Vec3i()
+	{	};
+	Vec3i( int X, int Y, int Z ):x(X), y(Y), z(Z)
+	{	};
+
+	// бинарные операторы
+	Vec3i operator + ( const Vec3i& ) const;
+	Vec3i operator - ( const Vec3i& ) const;
+	Vec3i operator * ( int ) const;
+	Vec3i operator / ( int ) const;
+	Vec3i operator + ( int ) const;
+	Vec3i operator - ( int ) const;
+};
+
+typedef class Vec3d
+{
+public:
+	double x;
+	double y;
+	double z;
+	Vec3d()
+	{	};
+	Vec3d( double X, double Y, double Z ):x(X), y(Y), z(Z)
+	{	};
+
+	// бинарные операторы
+	Vec3d operator + ( const Vec3d& ) const;
+	Vec3d operator - ( const Vec3d& ) const;
+	Vec3d operator * ( double ) const;
+	Vec3d operator / ( double ) const;
+	Vec3d operator + ( double ) const;
+	Vec3d operator - ( double ) const;
 };
 
