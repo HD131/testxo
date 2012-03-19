7cbfa3fe00f958a03df913501404ca7656bd5357
belovgarik <belovgarik@mail.ru>
2012-03-19 20:51:50 +0000

    разделение программы

--- Saper/src/Saper.cpp
+++ Saper/src/Saper.cpp
@@ -1,6 +1,8 @@
 #include "D3DDevice.h"
 #include "CameraDevice.h"
 #include "InputDevice.h"
+#include "Field.h"
+#include "Mesh.h"
 #include <vector>
 
 extern IDirect3DDevice9* g_pD3DDevice;
@@ -11,81 +13,6 @@ struct CSky
 	IDirect3DIndexBuffer9*  m_pBufIndexSky;
 	HRESULT                 InitialSky();
 	void                    Release();
-};
-
-class CMesh3D
-{
-public:
-	ID3DXMesh*							m_pMesh;
-	D3DMATERIAL9*						m_pMeshMaterial;
-	std::vector<IDirect3DTexture9*>     m_pMeshTextura;
-	DWORD								m_TexturCount; 
-	IDirect3DVertexBuffer9*				m_VertexBuffer;
-	IDirect3DIndexBuffer9*				m_IndexBuffer;
-	DWORD 								m_SizeFVF;
-	float								m_Alpha;
-	HRESULT								InitialMesh( LPCSTR Name, FILE* FileLog );
-	void								Release();
-	void								DrawMyMesh( ID3DXConstantTable** pConstTableVS, ID3DXConstantTable** pConstTablePS, 
-													IDirect3DVertexShader9** VertexShader, IDirect3DPixelShader9** PixelShader );
-	void								SetMatrixWorld( const D3DXMATRIX& Matrix );
-	void								SetMatrixView( const D3DXMATRIX& Matrix );
-	void								SetMatrixProjection( const D3DXMATRIX& Matrix );
-private:
-	D3DXMATRIX							m_MatrixWorld;
-	D3DXMATRIX							m_MatrixView;
-	D3DXMATRIX							m_MatrixProjection;
-};
-
-struct CField
-{
-	std::vector<CCell>   m_Cell;
-	std::vector<int>     m_Field;
-	CField( int Size )
-	{
-		m_Cell.resize(Size*Size);
-		m_Field.resize(Size*Size);
-		int x,y;
-		for ( y = 0; y < Size; ++y )
-			for ( x = 0; x < Size; ++x )
-			{
-				m_Cell[x*Size+y].SetCenter(  y - int((Size-1)/2) , 0 ,x - int((Size-1)/2));	
-				m_Cell[x*Size+y].m_Value = Empty;
-				m_Field[x*Size+y]        = Empty;
-			}
-		int mine = 0;
-		srand( time(0) );
-		while ( mine < MaxMine )
-		{
-			x = rand() % Size;
-			y = rand() % Size;
-			if ( m_Cell[x*Size+y].m_Value == Empty )
-			{
-				m_Cell[x*Size+y].m_Value = Mine;
-				++mine;
-			}
-		}			
-		for ( y = 0; y < Size; ++y )
-			for ( x = 0; x < Size; ++x )
-			{
-				mine = 0;
-				if (  m_Cell[x*Size+y].m_Value == Empty )
-				{					
-					for ( int a = x - 1; a < x + 2; ++a )
-						for ( int b = y - 1; b < y + 2; ++b)						
-							if (  (a >=0) && (b >= 0) && (a < Size) && (b < Size) && (m_Cell[a*Size+b].m_Value == Mine) )							
-								++mine;	
-											
-				}
-				if ( mine > 0)
-					m_Cell[x*Size+y].m_Value = mine;
-		   }
-	}
-   ~CField()
-   {
-		m_Cell.~vector();
-		m_Field.~vector();
-   }
 };
 
 
@@ -99,7 +26,7 @@ CMesh3D		 g_MeshWin;
 CMesh3D		 g_MeshLost;
 CMesh3D      g_MeshStalemate;
 CameraDevice g_Camera;
-CField       g_Field( MaxField);
+CField       g_Field( MaxField );
 bool         g_Exit      = false;
 bool		 g_Wireframe = false;
 
@@ -699,133 +626,3 @@ void CSky::Release()
 		m_pVerBufSky -> Release();
 }
 
-HRESULT CMesh3D::InitialMesh(LPCSTR Name, FILE *FileLog )
-{
-	m_pMesh         = 0;
-	m_pMeshMaterial = 0;
-	m_pMeshTextura.resize(0);
-	m_SizeFVF       = 0;
-	m_Alpha         = 1.0f;	
-	ID3DXBuffer *pMaterialBuffer  = 0;
-	if ( FAILED( D3DXLoadMeshFromX( Name, D3DXMESH_SYSTEMMEM, g_pD3DDevice, 0, &pMaterialBuffer, 0, &m_TexturCount, &m_pMesh ) ) )
-	{
-		if ( m_pMesh == 0 )
-		{		
-			if ( FileLog ) 
-				fprintf( FileLog, "error load x-file '%s'\n", Name );
-			return E_FAIL;
-		}
-	}
-
-	if ( m_pMesh->GetFVF() & D3DFVF_XYZ ) 
-		m_SizeFVF += sizeof(float)*3;
-	if ( m_pMesh->GetFVF() & D3DFVF_NORMAL ) 
-		m_SizeFVF += sizeof(float)*3;
-	if ( m_pMesh->GetFVF() & D3DFVF_TEX1 )
-		m_SizeFVF += sizeof(float)*2;
-
-	m_pMesh->GetVertexBuffer( &m_VertexBuffer );
-	m_pMesh->GetIndexBuffer(  &m_IndexBuffer  );
-	// Извлекаем свойства материала и названия{имена} структуры
-	D3DXMATERIAL *MaterialMesh = (D3DXMATERIAL *)pMaterialBuffer->GetBufferPointer();
-	m_pMeshMaterial  = new D3DMATERIAL9[m_TexturCount];
-	
-	for ( DWORD i = 0; i < m_TexturCount; i++ )
-	{
-		// Копируем материал
-		m_pMeshMaterial[i] = MaterialMesh[i].MatD3D;
-		// Установить окружающего свет
-		m_pMeshMaterial[i].Ambient = m_pMeshMaterial[i].Diffuse;
-		// Загружаем текстуру
-		if( MaterialMesh[i].pTextureFilename != 0 )
-		{		
-			IDirect3DTexture9* Tex = 0;
-			std::string FileName = std::string( "model//" ) + std::string( MaterialMesh[i].pTextureFilename );
-			if ( FAILED( D3DXCreateTextureFromFile( g_pD3DDevice, FileName.c_str(), &Tex )))
-			{
-				fprintf( FileLog, "error load texture '%s'\n", MaterialMesh[i].pTextureFilename );
-				m_pMeshTextura.push_back(0);
-			}
-			// Сохраняем загруженную текстуру
-			m_pMeshTextura.push_back(Tex);
-		}
-		else 
-		{
-			// Нет текстуры для i-ой подгруппы
-			m_pMeshTextura.push_back(0);
-		}
-	}
-	// Уничтожаем буфер материала
-	pMaterialBuffer->Release();
-
-	return S_OK;
-}
-
-void CMesh3D::SetMatrixWorld( const D3DXMATRIX& Matrix )
-{
-	m_MatrixWorld = Matrix;
-}
-
-void CMesh3D::SetMatrixView( const D3DXMATRIX& Matrix )
-{
-	m_MatrixView = Matrix;
-}
-
-void CMesh3D::SetMatrixProjection( const D3DXMATRIX& Matrix )
-{
-	m_MatrixProjection = Matrix;
-}
-void CMesh3D::DrawMyMesh( ID3DXConstantTable**     pConstTableVS, ID3DXConstantTable**       pConstTablePS,
-						  IDirect3DVertexShader9** VertexShader, IDirect3DPixelShader9**     PixelShader )
-{
-	D3DXMATRIX  wvp;
-	if ( m_pMesh )
-	{
-		wvp = m_MatrixWorld * m_MatrixView * m_MatrixProjection;
-		if ( pConstTableVS[Diffuse] )
-		{
-			pConstTableVS[Diffuse]->SetMatrix( g_pD3DDevice, "mat_mvp",   &wvp );
-			pConstTableVS[Diffuse]->SetMatrix( g_pD3DDevice, "mat_world", &m_MatrixWorld );
-			pConstTableVS[Diffuse]->SetVector( g_pD3DDevice, "vec_light", &g_Light );
-			pConstTablePS[Diffuse]->SetFloat(  g_pD3DDevice, "diffuse_intensity", g_Diffuse_intensity );	
-			pConstTablePS[Diffuse]->SetFloat(  g_pD3DDevice, "Alpha", m_Alpha );	
-		}
-		// устанавливаем шейдеры
-		g_pD3DDevice->SetVertexShader( VertexShader[Diffuse] );
-		g_pD3DDevice->SetPixelShader(  PixelShader [Diffuse] );
-
-		g_pD3DDevice->SetStreamSource( 0, m_VertexBuffer, 0, m_SizeFVF );
-		g_pD3DDevice->SetIndices( m_IndexBuffer );
-		for ( int i = 0; i < m_TexturCount; ++i )
-		{
-			g_pD3DDevice -> SetMaterial( &m_pMeshMaterial[i] );
-			g_pD3DDevice -> SetTexture( 0, m_pMeshTextura[i] );
-			//m_pMesh -> DrawSubset(i);
-		}
-		g_pD3DDevice->DrawIndexedPrimitive( D3DPT_TRIANGLELIST, 0, 0, m_pMesh->GetNumVertices(), 0, m_pMesh->GetNumFaces() ); 
-	}
-}
-
-void CMesh3D::Release()
-{
-	if ( m_IndexBuffer )
-		m_IndexBuffer->Release();
-	if ( m_VertexBuffer )
-		m_VertexBuffer->Release();
-
-	if ( m_pMeshMaterial )
-		delete[] m_pMeshMaterial;
-	/*
-	if ( m_pMeshTextura )
-	{
-		for ( int i = 1; i < m_TexturCount; ++i )
-		{
-			if ( m_pMeshTextura[i] )
-				m_pMeshTextura[i] -> Release();
-		}
-		delete []m_pMeshTextura;
-	}
-	*/
-	if ( m_pMesh )
-		m_pMesh -> Release();
-}
